53ed2f7efe2d5085f1d2f7bff2930e9b33eabf52
Brenda Dang <bdan0005@student.monash.edu>
2025-06-17 04:52:04 +1000

    Add center module for survey, fix padding

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import TopBar from './components/TopBar';
+import Module from './components/Module';
 
 function App() {
   return (
     <Router>
-      <div className="p-4">
-        <TopBar />
+      <div className="px-10 py-5">
+        <div className="pb-5">
+          <TopBar />
+        </div>
       </div>
     </Router>
   );
---
+++ frontend/src/components/Module.jsx
@@ -0,0 +1,11 @@
+const Module = () => {
+  return (
+    <div className="bg-gradient-to-br from-light-blue to-white text-white p-10 rounded-2xl shadow-sm">
+      <div className="text-black">
+        Engineering Pathways Navigator
+      </div>
+    </div>
+  );
+};
+
+export default Module;
