2c15734538e363f3ad935408f473772ba21df201
Brenda Dang <bdan0005@student.monash.edu>
2025-08-17 20:45:59 +1000

    Make base survey module

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,16 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import TopBar from './components/TopBar';
+import Module from './components/Module';
 
 function App() {
   return (
     <Router>
       <div className="px-10 py-5">
         <div className="pb-5">
-          <TopBar />
+          <div className="pb-5">
+            <TopBar />
+          </div>
+          <Module />
         </div>
       </div>
     </Router>
