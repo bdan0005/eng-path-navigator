fb3d2504ebdb73edb7c069a96d34be168826cadb
Victor Huang <rotciv3210@gmail.com>
2025-08-19 18:10:20 +1000

    Changing the accent colour of the slider and allowing width changes

--- frontend/src/components/Slider.jsx
+++ frontend/src/components/Slider.jsx
@@ -1,21 +1,21 @@
-import { useState } from "react";
-
 function Slider({
-    min,
-    max,
-    value,
-    onChange
+  min,
+  max,
+  value,
+  width = "600px",
+  onChange,
 }) {
   return (
-    <div>
+    <div className="flex items-center">
       <input
         type="range"
         min={min}
         max={max}
         value={value}
+        style={{ width: width }}
         onChange={e => onChange(Number(e.target.value))}
       />
-      <span>{value}</span>
+      <span className="ml-2 text-black text-base">{value}</span>
     </div>
   );
 }
