abb4ca4588824466a92c6affb2a5b7e0ef112121
Brenda Dang <bdan0005@student.monash.edu>
2025-10-15 03:59:23 +1100

    Add logo

--- frontend/src/components/TopBar.jsx
+++ frontend/src/components/TopBar.jsx
@@ -1,11 +1,10 @@
 import { Link } from 'react-router-dom';
 import Button from './Button';
-import Logo from '../assets/temp_logo.png';
 
 const TopBar = () => {
   return (
     <div className="w-full bg-white shadow-sm px-20 py-4 rounded-2xl flex items-center justify-between">
-      <img src={Logo} alt="Logo" className="h-12 w-auto" />
+      <img src="/temp_logo.png" alt="Logo" className="h-12 w-auto" />
       <Link to="/">
         <Button 
           type="secondary" 
