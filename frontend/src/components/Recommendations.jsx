67a2ab60db1453e6e3ecf6f6d071623bda1163e9
Victor Huang <rotciv3210@gmail.com>
2025-10-15 14:24:52 +1100

    fixing the issue of the specialisations not showing

--- frontend/src/components/Recommendations.jsx
+++ frontend/src/components/Recommendations.jsx
@@ -16,8 +16,8 @@ const Recommendations = ({ specialisations }) => {
       </div>
 
       <div className="grid grid-rows-7 gap-2 sm:p-1 md:p-5">
-        {rest.map(({spec, prob}, idx) => (
-          <SpecialisationRow key={idx} name={spec} prob={prob} />
+        {rest.map(({ specialisation, probability }, idx) => (
+          <SpecialisationRow key={idx} name={specialisation} prob={probability} />
         ))}
       </div>
     </div>
